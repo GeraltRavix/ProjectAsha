bf89b0c65a1dba71eb8319cbccc818389eb27857
GeraltRavix <453675@students.wits.ac.za>
2017-04-28 07:31:02 +0200

    Add files via upload

---
+++ CoinSpawn.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinSpawn : MonoBehaviour {
+
+    public GameObject Coin;
+    private Vector3 BuildingLocation;
+    private bool IsBuilding;
+    private float TargetTime = 5;
+	// Use this for initialization
+	void Start ()
+    {
+        BuildingLocation = GameObject.FindGameObjectWithTag("Building").transform.position;
+        IsBuilding = GameObject.FindGameObjectWithTag("Building");
+        //Instantiate(Coin, new Vector3(0, 0, 0), Quaternion.identity);
+        Instantiate(Coin, BuildingLocation, Quaternion.identity);
+    }
+
+
+    // Update is called once per frame
+    void Update ()
+    {
+        BuildingLocation = GameObject.FindGameObjectWithTag("Building").transform.position;
+        IsBuilding = GameObject.FindGameObjectWithTag("Building");
+        //print(IsBuilding);
+        if (IsBuilding == true)
+        {
+            Instantiate(Coin, BuildingLocation, Quaternion.identity);
+            IsBuilding = false;
+        }
+        //print(IsBuilding);
+
+        //Timer = Time.time + 5;
+        //print(Timer);
+        //print(TargetTime);
+        //print(Time.time);
+        if(TargetTime >= Time.time)
+        {
+            Instantiate(Coin, BuildingLocation, Quaternion.identity);
+            TargetTime = Time.time + 5;
+            
+            //print("test");
+        }
+
+
+        
+		
+	}
+
+}
---
+++ PlayerMove.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.AI;
+
+public class PlayerMove : MonoBehaviour
+{
+    //Vector3 newPosition;
+    //public float speed;
+    //private Vector3 target;
+    //void Start()
+    //{
+    //    newPosition = transform.position;
+    //}
+    //void Update()
+    //{
+    //    if (Input.GetMouseButtonDown(0))
+    //    {
+    //        RaycastHit hit;
+    //        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    //        if (Physics.Raycast(ray, out hit))
+    //        {
+    //            newPosition = hit.point;
+    //            transform.position = newPosition;
+    //            target = transform.position;
+    //            //target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    //            transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+
+
+
+    //        }
+    //        //target.z = transform.position.z;
+    //        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+
+    //    }
+    //}
+
+    public float speed;
+    private Vector3 target;
+
+    void Start()
+    {
+        target = transform.position;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            target.z = transform.position.z;
+        }
+        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+    }
+
+}
